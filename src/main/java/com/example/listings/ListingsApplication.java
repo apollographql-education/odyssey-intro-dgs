a66f7b2385883eb3ab8207518cd7b9c11fc971a2
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-05-17 13:42:30 -0600

    first commit

---
+++ src/main/java/com/example/listings/ListingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.listings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/listings/ListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
