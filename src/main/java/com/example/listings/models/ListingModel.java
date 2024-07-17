392f0746289ca12dca6340ecb9a6045f97dace35
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-07-17 15:06:33 -0600

    lesson 8: connecting datafetcher to the data source and returning live data

--- src/main/java/com/example/listings/models/ListingModel.java
+++ src/main/java/com/example/listings/models/ListingModel.java
@@ -1,6 +1,8 @@
 package com.example.listings.models;
 
 import com.example.listings.generated.types.Listing;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ListingModel extends Listing {
 }
